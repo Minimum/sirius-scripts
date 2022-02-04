9bce96735c47bdb35c411bb48f26c6c288485e03
Matt Ross <mross@gslans.net>
2022-02-03 22:57:47 -0800

    Use absolute paths for commands in check disk status

--- disk/check_status.sh
+++ disk/check_status.sh
@@ -20,7 +20,7 @@ echo "<pre>" >> $l_log_path
 # ZFS Pool Status
 echo "=== ZFS Pool Status ===" >> $l_log_path
 echo "" >> $l_log_path
-zpool status >> $l_log_path
+/usr/sbin/zpool status >> $l_log_path
 
 # SMART Status
 echo "" >> $l_log_path
@@ -28,37 +28,37 @@ echo "=== SMART Status ===" >> $l_log_path
 echo "" >> $l_log_path
 
 echo "/dev/nvme0n1:" >> $l_log_path
-smartctl -H "/dev/nvme0n1" >> $l_log_path
+/usr/sbin/smartctl -H "/dev/nvme0n1" >> $l_log_path
 
 echo "/dev/sda:" >> $l_log_path
-smartctl -H "/dev/sda" >> $l_log_path
+/usr/sbin/smartctl -H "/dev/sda" >> $l_log_path
 
 echo "/dev/sdb:" >> $l_log_path
-smartctl -H "/dev/sdb" >> $l_log_path
+/usr/sbin/smartctl -H "/dev/sdb" >> $l_log_path
 
 echo "/dev/sdc:" >> $l_log_path
-smartctl -H "/dev/sdc" >> $l_log_path
+/usr/sbin/smartctl -H "/dev/sdc" >> $l_log_path
 
 echo "/dev/sdd:" >> $l_log_path
-smartctl -H "/dev/sdd" >> $l_log_path
+/usr/sbin/smartctl -H "/dev/sdd" >> $l_log_path
 
 echo "/dev/sde:" >> $l_log_path
-smartctl -H "/dev/sde" >> $l_log_path
+/usr/sbin/smartctl -H "/dev/sde" >> $l_log_path
 
 echo "/dev/sdf:" >> $l_log_path
-smartctl -H "/dev/sdf" >> $l_log_path
+/usr/sbin/smartctl -H "/dev/sdf" >> $l_log_path
 
 echo "/dev/sdg:" >> $l_log_path
-smartctl -H "/dev/sdg" >> $l_log_path
+/usr/sbin/smartctl -H "/dev/sdg" >> $l_log_path
 
 echo "/dev/sdh:" >> $l_log_path
-smartctl -H "/dev/sdh" >> $l_log_path
+/usr/sbin/smartctl -H "/dev/sdh" >> $l_log_path
 
 # Close email
 echo "</pre></body></html>" >> $l_log_path
 
 # Send email
-msmtp "$l_email" < $l_log_path
+/usr/bin/msmtp "$l_email" < $l_log_path
 
 # Clear local variables
 unset l_log_path
